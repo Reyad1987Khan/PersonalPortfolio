0ebb72d3a0506bcb157324156e34b98022d4f026
Reyad Khan <khanreyad207@gmail.com>
2024-02-07 02:28:39 +0600

    Portfolio second commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// =============Toggle Icon Navbar Start Time: 51:44 ============= 
+let manuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+manuIcon.onclick = () =>{
+    // this two (bx-x and active class has been add dynamically by toggle method) 
+    manuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+// =============Toggle Icon Navbar End Time: 58:08 ============= 
+
+// =============Scroll Section Active Link Start Time: 46:30 ============= 
+
+// =============Scroll Section Active Link Start Time: 51:10 ============= 
+
